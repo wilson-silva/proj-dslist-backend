0093334a74d1ccc3a7dd21da50dd8a7167ddca57
wilson-silva <wilson-gs@hotmail.com>
2023-05-09 08:20:53 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,10 +21,14 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
@@ -37,7 +41,7 @@ public class Game {
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platforms;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -79,11 +83,11 @@ public class Game {
 	}
 
 	public String getPlatforms() {
-		return platform;
+		return platforms;
 	}
 
 	public void setPlatforms(String platforms) {
-		this.platform = platforms;
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
