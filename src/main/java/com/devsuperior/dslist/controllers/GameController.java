51634e046826c1d96ab229fc1b2a8375b6ae2d44
wilson-silva <wilson-gs@hotmail.com>
2023-05-09 22:02:09 -0300

    Exception, update service and controller

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,13 +1,19 @@
 package com.devsuperior.dslist.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 
 @RestController
@@ -21,7 +27,12 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll() {
 		return gameService.findAll();
-
 	}
+
+	// -------------------------------------------------------------------------
+	 @GetMapping("/{id}")
+	    public ResponseEntity<GameDTO> findById(@PathVariable Long id){
+	        return ResponseEntity.ok(new GameDTO(gameService.findById(id)));
+	    }
 
 }
--- src/main/java/com/devsuperior/dslist/dto/GameDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -1,52 +1,30 @@
-package com.devsuperior.dslist.entities;
+package com.devsuperior.dslist.dto;
 
 import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.beans.BeanUtils;
 
-@Entity
-@Table(name = "tb_game")
-public class Game {
+import com.devsuperior.dslist.entities.Game;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+public class GameDTO {
+
 	private Long id;
 	private String title;
-	
-	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-	
-	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	
-	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
-	public Game() {
+	public GameDTO() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
-			String shortDescription, String longDescription) {
-		this.id = id;
-		this.title = title;
-		this.year = year;
-		this.genre = genre;
-		this.platforms = platforms;
-		this.score = score;
-		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
 	}
-	
 	
 
 	public Long getId() {
@@ -121,21 +99,5 @@ public class Game {
 		this.longDescription = longDescription;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Game other = (Game) obj;
-		return Objects.equals(id, other.id);
-	}
 
 }
